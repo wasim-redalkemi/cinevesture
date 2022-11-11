1fb709923fb014d59ef516cc06c75133eef07dab
Anish pal <anish.pal@scaleupally.io>
2022-11-11 18:05:04 +0530

    category update

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Controller;
+use App\Models\MasterProjectCategory;
+use App\Models\ProjectCategory;
 use App\Models\UserProject;
 use Illuminate\Http\Request;
+use SebastianBergmann\CodeCoverage\Report\Xml\Project;
 
 class ProjectController extends AdminController
 {
@@ -126,5 +129,29 @@ class ProjectController extends AdminController
     public function destroy($id)
     {
         //
+    }
+
+    public function categoryUpdateView(request $request)
+    {
+        
+        $projectcategories = ProjectCategory::query()->where('project_id',$request->pid)->get();
+        $categories = MasterProjectCategory::get();
+        $temp_cat = [];
+        foreach($projectcategories as $k =>$v)
+        {
+            array_push($temp_cat,$v->category_id);
+        }
+        $projectcategories = $temp_cat;
+        return view('admin.project.categoryedit',compact(['projectcategories','categories']));
+    }
+    public function categoryUpdate(request $request)
+    {
+        dd($request);
+        $category=ProjectCategory::find();
+    }
+
+    public function queryUpdateView(request $request)
+    {
+        return view('admin.project.queryedit');
     }
 }
