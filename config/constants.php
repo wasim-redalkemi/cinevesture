4c26db1810d7299044a4d433619b0ad6710d204a
vaibhav vishnoi <vaibhav.vishnoi@scaleupally.io>
2022-11-23 16:28:53 +0530

    Resolved issue asset url in constant

--- config/constants.php
+++ config/constants.php
@@ -3,7 +3,7 @@
 return [
 
     "GOOGLE_API_KEY" => "AIzaSyCNJqNKLKFTBoLnbrbNtP8MDlz2vBVvNnE", //Suprabhat's account
-    "PROJECT_NO_IMAGE" => asset('public/images/asset/download (3) 2.png')
+    "PROJECT_NO_IMAGE" => 'public/images/asset/download (3) 2.png'
 ];
 
 ?>
