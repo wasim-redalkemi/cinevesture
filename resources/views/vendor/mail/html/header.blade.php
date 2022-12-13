0da38426b3137f6d9f23f398b0b095eab0be9e74
Prashant Negi <prashant.negi@scaleupally.io>
2022-12-13 16:26:08 +0530

    email template issue resolve

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,9 +1,9 @@
 <tr>
 <td class="header" style="padding:0">
     <div style="width: 100%;    background-color: #1C0330;" >
-    <a href="{{ $url }}" style="display: inline-block;">
-    <img style="width: 100%;" src="{{asset('images/asset/Logo-white-trans.png')}}" class="logo" alt="Cinevesture Logo">
-</a>
+    <!-- <a href="{{ $url }}" style="display: inline-block;"> -->
+    <img style="width: 100%; display:block; height:150px" src="{{asset('images/asset/Logo-white-trans.png')}}"  width ="150" height = "150" class="logo" alt="Cinevesture Logo">
+<!-- </a> -->
     </div>
 
 </td>
