3fed78b8a8f25bb933cb5acfe251227ed4d4ea68
Prashant Negi <prashant.negi@scaleupally.io>
2022-12-13 12:02:49 +0530

    changes in email logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header" style="padding:0">
     <div style="width: 100%;    background-color: #1C0330;" >
     <a href="{{ $url }}" style="display: inline-block;">
-    <img style="width: 100%;" src="http://3.6.144.86/cinevesture/images/asset/Logo-white-trans.png" class="logo" alt="Cinevesture Logo">
+    <img style="width: 100%;" src="{{asset('images/asset/Logo-white-trans.png')}}" class="logo" alt="Cinevesture Logo">
 </a>
     </div>
 
