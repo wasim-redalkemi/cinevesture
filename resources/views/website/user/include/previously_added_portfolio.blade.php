d87d0b678060f48e39e9e30acf06174b173df3b7
ScaleupAlly Dev <devscaleupally@gmail.com>
2022-12-20 17:47:29 +0530

    updated code for previous data added

---
+++ resources/views/website/user/include/previously_added_portfolio.blade.php
@@ -0,0 +1,73 @@
+<section>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="previous_data_wraper">
+          <div class="accordion" id="accordionPortfolio">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed collapse_text_design" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  Portfolio
+                </button>
+              </h2>
+              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionPortfolio">
+                <div class="my-3">
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="card previous_data_card">
+                          <div class="d-flex justify-content-between">
+                            <div class="crop_input_label">Name</div>
+                            <div>
+                              <i class="fa fa-pencil mx-2 Aubergine" aria-hidden="true"></i>
+                              <i class="fa fa-trash Aubergine" aria-hidden="true"></i>
+                            </div>
+                          </div>
+                          <div class="mt-2">
+                            <div class="upload_resume_text">Project Title</div>
+                            <div class="upload_resume_text mt-2">Project Title</div>
+                            <div class="upload_resume_text mt-2">Project Title</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="card previous_data_card">
+                          <div class="d-flex justify-content-between">
+                            <div class="crop_input_label">Name</div>
+                            <div>
+                              <i class="fa fa-pencil mx-2 Aubergine" aria-hidden="true"></i>
+                              <i class="fa fa-trash Aubergine" aria-hidden="true"></i>
+                            </div>
+                          </div>
+                          <div class="mt-2">
+                            <div class="upload_resume_text">Project Title</div>
+                            <div class="upload_resume_text mt-2">Project Title</div>
+                            <div class="upload_resume_text mt-2">Project Title</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="card previous_data_card">
+                          <div class="d-flex justify-content-between">
+                            <div class="crop_input_label">Name</div>
+                            <div>
+                              <i class="fa fa-pencil mx-2 Aubergine" aria-hidden="true"></i>
+                              <i class="fa fa-trash Aubergine" aria-hidden="true"></i>
+                            </div>
+                          </div>
+                          <div class="mt-2">
+                            <div class="upload_resume_text">Project Title</div>
+                            <div class="upload_resume_text mt-2">Project Title</div>
+                            <div class="upload_resume_text mt-2">Project Title</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
