3954aa53b8396533161a10afbbef425f93b493fa
kamal <kamal.sharma@scaleupally.io>
2022-12-28 17:34:25 +0530

    merge extra file

---
+++ resources/views/website/auth/guest_header.blade.php
@@ -0,0 +1,16 @@
+@section('header')
+<header class="Header_main_container">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <nav class="navbar navbar-expand-lg header">
+            
+            <a class="navbar-logo " href="{{route('home')}}">
+              <img src="{{ asset('images/asset/Logo-white-trans.png') }}" width="220" height="75" alt="image">
+            </a>
+        </div>
+        </nav>
+      </div>
+    </div>
+</header>
+@endsection
