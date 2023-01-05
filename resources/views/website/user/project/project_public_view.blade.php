2c6baf54ff6638a5e180b2a242675231c232f751
kamal <kamal.sharma@scaleupally.io>
2023-01-05 13:12:26 +0530

    fix modal

--- resources/views/website/user/project/project_public_view.blade.php
+++ resources/views/website/user/project/project_public_view.blade.php
@@ -162,67 +162,6 @@
                                         <button class="cantact-page-cmn-btn mt-2" data-toggle="modal" data-target="#contactModal">Contact Now </button>
                                     {{-- <button class="cantact-page-cmn-btn"><a href=""  class="text_decor_none">Contact Now</a></button> --}}
 
-
-
-                                        <!-- Contact modal  -->
-                                        <div class="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="ContactModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog" role="document">
-                                                <div class="modal-content bg_3308">
-                                                    <div class="modal-body p-0">
-                                                        <div class="p-3 float-end">
-                                                            <i class="fa fa-times text_fff font_24 pointer" data-dismiss="modal" aria-label="Close"></i>
-                                                        </div>
-                                                        <section class="p-3">
-                                                            <div class="container">
-                                                                <div class="row">
-                                                                    <div class="col-md-12">
-                                                                        <div class="signup-text  mb-4 mt-5"> Contact </div>
-                                                                        <div class="row">
-                                                                            <div class="col-md-3">
-                                                                                {{-- <div><img src="{{ asset('images/asset/photo-1595152452543-e5fc28ebc2b8 2.png') }}" class="w-100 br_100"></div> --}}
-                                                                                <?php
-                                                                                if (empty($projectData[0]['user']['profile_image'])) {
-                                                                                ?>
-                                                                                    <img src="{{ asset('images/asset/user-profile.png') }}" class="w-100 br_100">
-                                                                                <?php
-                                                                                } else {
-                                                                                ?>
-                                                                                    <img src="{{Storage::url($projectData[0]['user']['profile_image'])}}" class="w-100 br_100 " alt="product-image" style="height:100%;width:100%;">
-                                                                                <?php
-                                                                                }
-                                                                                ?>
-                                                                            </div>
-                                                                            <div class="col-md-9">
-                                                                                <div class="tile_text text_fff">{{empty($projectData[0]['user']['first_name'])?'Name':ucfirst($projectData[0]['user']['first_name']).' '.ucfirst($projectData[0]['user']['last_name']);}}</div>
-                                                                                <div class="organisation_cmn_text text_fff">{{empty($projectData[0]['user']['job_title'])?'Job Title':ucfirst($projectData[0]['user']['job_title']);}}</div>
-                                                                            </div>
-                                                                        </div>
-                                                                        <div class="mt-3"><input type="text" id="subject" name="subject" value="" placeholder="Subject" class="modal_input"></div>
-                                                                        <div class="mt-3">
-                                                                            <textarea name="message" id="message" cols="25" rows="6" class="w-100 modal_input controlTextLength" placeholder="Message" text-length = "1200" maxlength="1200" aria-label="With textarea"></textarea>
-
-                                                                        </div>
-
-                                                                        <div class="form-check mt-3">
-                                                                            <input class="form-check-input modal_check_input" type="checkbox" id="checkbox_cc">
-                                                                            <label class="modal_btm_text mx-1">Send a copy to me</label>
-                                                                        </div>
-
-                                                                        <div class="mt-4">
-                                                                            <input type="hidden" name="email_1" id="email_1" class="modal_input" value="@if (!empty($projectData[0]['user']['email'])){{$projectData[0]['user']['email']}}@endif">
-                                                                            <button type="button" id="contact_btn" class="invite_btn">Send Mail</button>
-                                                                        </div>
-                                                                        <div class="modal_btm_text mt-4 mb-5">
-                                                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum vel cras vitae morbi varius vitae.
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </section>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
                                     @endif
                                     <i class="fa fa-share-alt mx-4 icon-size" aria-hidden="true"></i>
                                     @if ($projectData[0]['user']['id'] != auth()->user()->id)
@@ -239,9 +178,9 @@
                     </div>
                 </div>
             </div>
-            </div>
-            </div>
-            </section>
+        </div>
+    </div>
+</section>
 
 <section class="public_section">
     <div class="container">
