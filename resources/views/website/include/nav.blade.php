c63bcb832ea813d3d1ac7df3b38cfa4c76b76d23
ScaleupAlly Dev <devscaleupally@gmail.com>
2023-02-09 18:54:29 +0530

    updated code

--- resources/views/website/include/nav.blade.php
+++ resources/views/website/include/nav.blade.php
@@ -1,12 +1,12 @@
 @section('nav')
-<div class="" >
+<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 
 <nav class="navbar navbar-expand-lg sub-header">
     <div class="container">
       <div class="row w_100">
         <div class="col-md-12">
           <div class="navbar_wraper_sm">
-          <div class="collapse navbar-collapse justify-content-between"  id="navbarTogglerDemo02">
+          <div class=" justify-content-between"  >
             <div>
             <form class="" id ="filter" method="GET" action="{{ route('get-project-filter') }}">
                         @csrf
