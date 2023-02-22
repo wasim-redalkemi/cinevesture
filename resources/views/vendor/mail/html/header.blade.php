3fda7ab76dcfc9cbaa8e962272ec54ec8b402366
Prashant Negi <prashant.negi@scaleupally.io>
2023-02-22 14:06:20 +0530

    chnage logo width for email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header" style="padding:0">
     <div style="width: 100%;    background-color: #1C0330;" >
     <a href="{{ $url }}" style="display: inline-block;">
-    <img src="{{asset('images/asset/Logo-white-trans.png')}}"  width ="150"  class="logo" alt="Cinevesture Logo">
+    <img src="{{asset('images/asset/Logo-white-trans.png')}}"  width ="250"  class="logo" alt="Cinevesture Logo">
 </a>
     </div>
 
