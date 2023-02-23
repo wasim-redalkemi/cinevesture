fc0521c13d0877060014a5252bc82980a14dde82
vaibhav vishnoi <vaibhav.vishnoi@scaleupally.io>
2023-02-23 16:22:54 +0530

    Resolved filter logo and give some padding

--- resources/views/website/include/header.blade.php
+++ resources/views/website/include/header.blade.php
@@ -54,8 +54,8 @@
               ?>
             </div>
               </div>
-              @if (!($currentPath == 'filter') && !($currentPath == 'results') && !($currentPath == 'profile-filter'))
-                
+              {{-- @if (!($currentPath == 'filter') && !($currentPath == 'results') && !($currentPath == 'profile-filter') && !($currentPath == 'search') && !($currentPath == 'profile-show')) --}}
+              @if ($currentPath == 'home')
               <button class="navbar-toggler mt-1" style="right: 40px;" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
               aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"><i class="fa fa-filter text-light" aria-hidden="true"></i></span>
