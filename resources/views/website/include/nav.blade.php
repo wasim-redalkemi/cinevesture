92b618546df70a89f5784ca1628679a6189b6acd
ScaleupAlly Dev <devscaleupally@gmail.com>
2023-02-24 15:36:01 +0530

    add sidebar for nav in mobile

--- resources/views/website/include/nav.blade.php
+++ resources/views/website/include/nav.blade.php
@@ -1,7 +1,7 @@
 @section('nav')
 <div class="" >
 
-<nav class="navbar navbar-expand-lg sub-header">
+<nav class="navbar navbar-expand-lg sub-header d-none d-md-block">
     <div class="container">
       <div class="row w_100">
         <div class="col-md-12">
@@ -167,6 +167,173 @@
       </div>
     </div>
   </nav>    
+
+  <!-- for sidebar  -->
+
+
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <!-- <h5 class="offcanvas-title" id="offcanvasExampleLabel">Cinevesture</h5> -->
+    <button type="button" class="btn-close text-white" data-bs-dismiss="offcanvas" aria-label="Close"><img src="{{ asset('images/asset/crossicon.png') }}" width="35" alt="image"></button>
+  </div>
+  <div class="offcanvas-body">
+  <div>
+            <form class="" id ="filter" method="GET" action="{{ route('get-project-filter') }}">
+                        @csrf
+                        <div class="d-flex d-md-none justify-content-between align-items-center">
+                <div class="header-search-form">
+                <div class="search"><button type="submit" class="searchButton">
+                    <i class="fa fa-search"></i>
+                  </button>
+                  <input type="text" name="search" id="search-project"class="searchTerm" placeholder="Search">
+                </div>
+              </div>
+              <a class="my-2 my-sm-0 mr-0 " href="{{ route('project-overview')}}">
+                <!-- Add a Project    -->
+
+                <button type="button" class="add-proj-btn text-white">
+                      Add a Project
+                  </button>
+
+              </a>
+              </div>
+              <div class="navbar_mobile">
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                  <div class="dropdown home-dropdown">
+                    <a class="btn dropdown-toggle nav-link" href="#" role="button" data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      Genres
+                    </a>
+                    <ul class="dropdown-menu nav-drop" id="list-gener">
+                      @foreach($geners as $gen)
+                      <li>
+                        <input class="form-check-input" type="checkbox" name="geners[]" value="{{$gen->id}}" id="flexCheckDefault">
+                        <label class="form-check-label mx-2" for="flexCheckDefault">
+                          {{$gen->name}}
+                        </label>
+                      </li>
+                      @endforeach
+                    </ul>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <div class="dropdown home-dropdown">
+                    <a class="btn dropdown-toggle nav-link" href="#" role="button" data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      Category
+                    </a>
+                    <ul class="dropdown-menu nav-drop" id="list-categories">
+                       @foreach($categories as $cate)
+                      <li>
+                        <input class="form-check-input" type="checkbox" value="{{$cate->id}}"  name="categories[]" id="flexCheckDefault">
+                        <label class="form-check-label mx-2" for="flexCheckDefault">
+                          {{$cate->name}}
+                        </label>
+                      </li>
+                      @endforeach
+                    </ul>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <div class="dropdown home-dropdown">
+                    <a class="btn nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      Looking for
+                    </a>
+                    <ul class="dropdown-menu nav-drop" id="list-lookingFor">
+                    @foreach($looking_for as $look)
+                      <li>
+                        <input class="form-check-input" type="checkbox" value="{{$look->id}}" name="looking_for[]" id="flexCheckDefault">
+                        <label class="form-check-label mx-2" for="flexCheckDefault">
+                          {{$look->name}}
+                        </label>
+                      </li>
+                      @endforeach
+                      
+                    </ul>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <div class="dropdown home-dropdown">
+                    <a class="btn nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      Project Stage
+                    </a>
+                    <ul class="dropdown-menu nav-drop" id="list-stages">
+                    @foreach($project_stages as $pro)
+                      <li>
+                        <input class="form-check-input" type="checkbox" value="{{$pro->id}}"  name="project_stages[]" id="flexCheckDefault">
+                        <label class="form-check-label mx-2" for="flexCheckDefault">
+                          {{$pro->name}}
+                        </label>
+                      </li>
+                      @endforeach
+                      
+                    </ul>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <div class="dropdown home-dropdown">
+                    <a class="btn nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      Country
+                    </a>
+                    <ul class="dropdown-menu nav-drop" id="list-countries">
+                    @foreach($countries as $con)
+                      <li>
+                        <input class="form-check-input" type="checkbox" value="{{$con->id}}" name="countries[]" id="flexCheckDefault">
+                        <label class="form-check-label mx-2" for="flexCheckDefault">
+                          {{$con->name}}
+                        </label>
+                      </li>
+                      @endforeach
+                     
+                    </ul>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <div class="dropdown home-dropdown">
+                    <a class="btn dropdown-toggle nav-link" href="#" role="button" data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      Language
+                    </a>
+                    <ul class="dropdown-menu nav-drop" id="list-language">
+                    @foreach($languages as $lang)
+                      <li>
+                        <input class="form-check-input" type="checkbox" name="languages[]" value="{{$lang->id}}" id="flexCheckDefault">
+                        <label class="form-check-label mx-2" for="flexCheckDefault">
+                          {{$lang->name}}
+                        </label>
+                      </li>
+                      @endforeach
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+              </div>
+            </div>
+            <div class="d-flex justify-content-center">
+              <div class="header-search-form d-none d-md-block">
+                <div class="search"><button type="submit" class="searchButton">
+                    <i class="fa fa-search"></i>
+                  </button>
+                  <input type="text" name="search" id="search-project" class="searchTerm" placeholder="Search">
+                </div>
+              </div>
+              <!-- <div class="d--flex"> -->
+                <button type="submit" class="header-search-btn ml_10 mt-3 mr_5">Search</button>
+                <a href="{{ route('project-overview')}}">
+                  <button type="button" class="add-proj-btn my-2 my-sm-0 mr-0 d-none d-md-block text-white">
+                      Add a Project
+                  </button>
+                </a>
+              <!-- </div> -->
+            </form>
+            </div>
+  </div>
+</div>
+
 </div>
 
 @endsection
